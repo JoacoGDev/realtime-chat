3a4ea963014c7fc36c15ce16c640697a6c821f55
JoacoGDev <joacogilp@gmail.com>
2026-02-17 18:12:29 -0300

    Modified: se modifica el archivo de estilos para adaptarse a diferentes pantallas

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+app.set('trust proxy', 1);
 const port = process.env.PORT ?? 3000;
 
 //middleware
